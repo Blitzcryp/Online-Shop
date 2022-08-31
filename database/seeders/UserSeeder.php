c5d84917aeb79164e49d0a87289b67fdcf566a85
Coman Cosmin <comancosmin1112@gmail.com>
2022-08-31 20:16:53 +0300

    Add first user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            "name" => "Coman Cosmin",
+            "email" => "comancosmin112+ticketApp@gmail.com",
+            "email_verified_at" => Carbon::now()->addHour()->toDateTimeString(),
+            "password" => Hash::make("parola12345"),
+            "remember_token" => Hash::make("parola54321"),
+            "created_at" => Carbon::now()->toDateTimeString(),
+            "updated_at" => Carbon::now()->toDateTimeString(),
+        ]);
+    }
+}
